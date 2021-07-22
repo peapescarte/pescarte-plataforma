d13a156082c8854f3438b0d5d4839765d40b42af
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-07-22 18:22:37 -0300

    fix: continue setup sentry

--- mix.exs
+++ mix.exs
@@ -4,10 +4,11 @@ defmodule Fuschia.MixProject do
   def project do
     [
       app: :fuschia,
-      version: "0.1.0",
-      elixir: "~> 1.7",
+      version: "0.0.1",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      elixirc_options: [warnings_as_errors: true],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
@@ -43,6 +44,7 @@ defmodule Fuschia.MixProject do
       {:phoenix, "~> 1.5.9"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
+      {:hackney, "~> 1.8"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_live_dashboard, "~> 0.4"},
       {:sentry, "~> 8.0"},
@@ -50,7 +52,7 @@ defmodule Fuschia.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
+      {:plug_cowboy, "~> 2.3"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:git_hooks, "~> 0.6.3", only: [:dev], runtime: false}
