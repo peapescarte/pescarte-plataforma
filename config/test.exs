0a56dd64c8b5c0e33fd556f98b5ae3580e171b4a
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-07-22 18:02:04 -0300

    feat: set up new config style and git hooks

--- config/test.exs
+++ config/test.exs
@@ -1,15 +1,10 @@
-use Mix.Config
+import Config
 
-# Configure your database
-#
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
 config :fuschia, Fuschia.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "pescarte",
+  password: "pescarte",
   database: "fuschia_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: "db",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
