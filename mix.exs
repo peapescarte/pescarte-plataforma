8ce3410c4d0c442e897a739a9114a7c198a8d3c5
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-07-23 10:40:39 -0300

    fix: update readme, add coverage report

--- mix.exs
+++ mix.exs
@@ -59,7 +59,7 @@ defmodule Fuschia.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.3"},
-      {:excoveralls, "~> 0.10", only: :test},
+      {:excoveralls, "~> 0.10", only: [:dev, :test]},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:git_hooks, "~> 0.6.3", only: [:dev], runtime: false}
