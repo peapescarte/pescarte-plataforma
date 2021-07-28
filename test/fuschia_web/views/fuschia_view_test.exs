cbe4e4ec2119d60f791ce10278fbb3871765fd5c
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-07-28 13:17:26 -0300

    feat: fuschia view and view tests

---
+++ test/fuschia_web/views/fuschia_view_test.exs
@@ -0,0 +1,21 @@
+defmodule FuschiaWeb.FuschiaViewTest do
+  use FuschiaWeb.ConnCase, async: true
+
+  import Phoenix.View
+
+  alias FuschiaWeb.FuschiaView
+
+  test "renders response.json" do
+    data = %{test: true}
+    assert render(FuschiaView, "response.json", %{data: data}) == %{data: %{test: true}}
+  end
+
+  test "renders paginated.json" do
+    response = %{data: [%{test: true}], pagination: %{test: true}}
+
+    assert render(FuschiaView, "paginated.json", response) == %{
+             data: [%{test: true}],
+             pagination: %{test: true}
+           }
+  end
+end
