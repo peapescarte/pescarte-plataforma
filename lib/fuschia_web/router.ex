5616a627be219ce0aaf51be7f068caf538d59512
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-07-28 12:57:34 -0300

    feat: swagger routes

--- lib/fuschia_web/router.ex
+++ lib/fuschia_web/router.ex
@@ -1,10 +1,31 @@
 defmodule FuschiaWeb.Router do
   use FuschiaWeb, :router
 
+  pipeline :browser do
+    plug :accepts, ["html"]
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
     plug FuschiaWeb.LocalePlug
     plug ProperCase.Plug.SnakeCaseParams
+  end
+
+  pipeline :api_swagger do
+    plug :accepts, ["json"]
+    plug OpenApiSpex.Plug.PutApiSpec, module: FuschiaWeb.Swagger.ApiSpec
+  end
+
+  scope "/" do
+    pipe_through :browser
+
+    get "/swaggerui", OpenApiSpex.Plug.SwaggerUI, path: "/api/openapi"
+  end
+
+  scope "/api" do
+    pipe_through :api_swagger
+
+    get "/openapi", OpenApiSpex.Plug.RenderSpec, []
   end
 
   scope "/api", FuschiaWeb do
