73c891a8f66574850e86d0afb5a2d3e1b08a58b1
Eduardo R <link2755@hotmail.com>
2021-07-31 15:58:00 -0300

    feat: nucleo schema

---
+++ lib/fuschia/entities/nucleo.ex
@@ -0,0 +1,31 @@
+defmodule Fuschia.PesquisadorTables.Nucleo do
+  @moduledoc """
+  Esquema que representa um nucleo de Pesquisa no BD
+  """
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:nome, :string, []}
+  schema "nucleo" do
+    field :descricao_nucleo, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(nucleo, attrs) do
+    nucleo
+    |> cast(attrs, [:nome, :descricao_nucleo])
+    |> validate_required([:nome, :descricao_nucleo])
+  end
+
+  defimpl Jason.Encoder, for: __MODULE__ do
+    def encode(struct, opts) do
+      %{
+        nome: struct.nome,
+        descricao_nucleo: struct.descricao_nucleo
+      }
+      |> Fuschia.Encoder.encode(opts)
+    end
+  end
+end
