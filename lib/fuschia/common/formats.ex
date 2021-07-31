6a2221e84e765cd71d3d5f741cdf506423347936
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-07-31 00:34:02 -0300

    fix: remove unecessary format regex

--- lib/fuschia/common/formats.ex
+++ lib/fuschia/common/formats.ex
@@ -7,12 +7,9 @@ defmodule Fuschia.Common.Formats do
   @cnpj_format ~r/^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/
   @rg_format ~r/^\d{2}\.\d{3}\.\d{3}\-\d{1}$/
   @mobile_format ~r/^\(\d{2}\)(\d{5}|\s\d{5})-\d{4}$/
-  @cep_format ~r/^\d{5}\-\d{3}$/
-  @email_format ~r/^[^\s]+@[^\s]+$/
 
   def cpf, do: @cpf_format
   def rg, do: @rg_format
   def cnpj, do: @cnpj_format
   def mobile, do: @mobile_format
-  def email, do: @email_format
 end
