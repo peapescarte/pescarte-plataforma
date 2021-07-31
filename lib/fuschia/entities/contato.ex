f4bc30c6a22ed088d9a692b3a0c6a59a88bf9c2f
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-07-31 00:33:32 -0300

    feat: contato schema and migration

---
+++ lib/fuschia/entities/contato.ex
@@ -0,0 +1,65 @@
+defmodule Fuschia.Entities.Contato do
+  @moduledoc """
+  Contato schema
+  """
+
+  use Fuschia.Schema
+
+  import Ecto.Changeset
+
+  alias Fuschia.Common.Formats
+
+  @required_fields ~w(email endereco celular)a
+
+  @mobile_format Formats.mobile()
+
+  schema "contato" do
+    field :celular, :string
+    field :email, :string
+    field :endereco, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(contato, attrs) do
+    contato
+    |> cast(attrs, @required_fields)
+    |> validate_required(@required_fields)
+    |> validate_format(:celular, @mobile_format)
+    |> validate_email()
+  end
+
+  @doc """
+  A contact changeset for changing the email.
+
+  It requires the email to change otherwise an error is added.
+  """
+  def email_changeset(contact, attrs) do
+    contact
+    |> cast(attrs, [:email])
+    |> validate_email()
+    |> case do
+      %{changes: %{email: _}} = changeset -> changeset
+      %{} = changeset -> add_error(changeset, :email, "did not change")
+    end
+  end
+
+  defp validate_email(changeset) do
+    changeset
+    |> validate_length(:email, max: 160)
+    |> unsafe_validate_unique(:email, Fuschia.Repo)
+  end
+
+  defimpl Jason.Encoder, for: __MODULE__ do
+    def encode(struct, opts) do
+      %{
+        id: struct.id,
+        celular: struct.celular,
+        email: struct.email,
+        endereco: struct.endereco
+      }
+      |> Fuschia.Encoder.encode(opts)
+    end
+  end
+end
