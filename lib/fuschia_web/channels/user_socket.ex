2401723088c033480197403025b2c3d58aeed8bf
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-08-02 11:26:42 -0300

    fix: use `:cpf` as user primary key

--- lib/fuschia_web/channels/user_socket.ex
+++ lib/fuschia_web/channels/user_socket.ex
@@ -9,7 +9,7 @@ defmodule FuschiaWeb.UserSocket do
   # verification, you can put default assigns into
   # the socket that will be set for all channels, ie
   #
-  #     {:ok, assign(socket, :user_id, verified_user_id)}
+  #     {:ok, assign(socket, :user_cpf, verified_user_cpf)}
   #
   # To deny connection, return `:error`.
   #
@@ -22,12 +22,12 @@ defmodule FuschiaWeb.UserSocket do
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
   #
-  #     def id(socket), do: "user_socket:#{socket.assigns.user_id}"
+  #     def id(socket), do: "user_socket:#{socket.assigns.user_cpf}"
   #
   # Would allow you to broadcast a "disconnect" event and terminate
   # all active sockets and channels for a given user:
   #
-  #     FuschiaWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
+  #     FuschiaWeb.Endpoint.broadcast("user_socket:#{user.cpf}", "disconnect", %{})
   #
   # Returning `nil` makes this socket anonymous.
   @impl true
