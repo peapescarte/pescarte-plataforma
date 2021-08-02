05762cfd5d42f208e83930e3156d9771cdea9d80
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-08-02 12:17:21 -0300

    feat: mailer libs and structure

--- config/runtime.exs
+++ config/runtime.exs
@@ -30,6 +30,37 @@ config :fuschia, Oban,
 config :fuschia, :jobs, start: System.get_env("START_OBAN_JOBS", "true")
 
 # ---------------------------#
+# Mailer
+# ---------------------------#
+adapter =
+  case System.get_env("MAIL_SERVICE", "local") do
+    "mailgun" -> Swoosh.Adapters.Mailgun
+    "gmail" -> Swoosh.Adapters.SMTP
+    _ -> Swoosh.Adapters.Local
+  end
+
+if adapter == Swoosh.Adapters.Local do
+  config :swoosh, serve_mailbox: true, preview_port: 4001
+end
+
+config :fuschia, Mailer,
+  adapter: adapter,
+  domain: System.get_env("MAILGUN_DOMAIN", "email.uenf.br"),
+  api_key: System.get_env("MAILGUN_PRIVATE_API_KEY"),
+  base_url: System.get_env("MAILGUN_BASE_URL"),
+  relay: System.get_env("MAIL_SERVER", "smtp.gmail.com"),
+  username: System.get_env("MAIL_USERNAME", "notificacoes-noreply@peapescarte.uenf.br"),
+  password: System.get_env("MAIL_PASSWORD", ""),
+  ssl: false,
+  tls: :always,
+  auth: :always,
+  port: System.get_env("MAIL_PORT", "587")
+
+config :fuschia, :pea_pescarte_contact,
+  notifications_mail: "notifications-noreply@peapescarte.uenf.br",
+  telephone: "(11) 3038-1738"
+
+# ---------------------------#
 # Timex
 # ---------------------------#
 config :timex, timezone: System.get_env("TIMEZONE", "America/Sao_Paulo")
