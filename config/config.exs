e5d51db632f5c8bf9982b0425b385c4439f4ba86
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-08-03 00:10:43 -0300

    feat: move some configs to `runtime.exs`

--- config/config.exs
+++ config/config.exs
@@ -1,12 +1,17 @@
 import Config
 
+# ---------------------------#
+# Ecto
+# ---------------------------#
 config :fuschia,
   ecto_repos: [Fuschia.Repo]
 
 config :fuschia, Fuschia.Repo,
   migration_timestamps: [inserted_at: :created_at, type: :utc_datetime_usec]
 
-# Configures the endpoint
+# ---------------------------#
+# Endpoint
+# ---------------------------#
 config :fuschia, FuschiaWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "/tnqEz6BgkvSQoZdVePI7wI2tB6enxAPY66OSNNCGSeDy2VkzG0lIc/cguFxfA+0",
@@ -14,19 +19,11 @@ config :fuschia, FuschiaWeb.Endpoint,
   pubsub_server: Fuschia.PubSub,
   live_view: [signing_salt: "kHky3tuz"]
 
-# Configures Elixir's Logger
-config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id],
-  backends: [:console, Sentry.LoggerBackend]
-
-config :fuschia, FuschiaWeb.Auth.Guardian,
-  issuer: "pea_pescarte",
-  ttl: {3, :days},
-  secret_key: "BGfPrVpwxq8wpwuRRIyMQtihongnh98GAQPl0awRxCn432HLit9Wo/Q83yrjHH2P"
-
-# Use Jason for JSON parsing in Phoenix
+# ---------------------------#
+# Phoenix
+# ---------------------------#
 config :phoenix, :json_library, Jason
+config :phoenix, :format_encoders, json: ProperCase.JSONEncoder.CamelCase
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
