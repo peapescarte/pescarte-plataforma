fed59b6f97a9b247a9f10818b1923f75505de19a
Eduardo R <link2755@hotmail.com>
2021-08-09 11:23:50 -0300

    fix: base PR fixes

--- lib/fuschia/entities/pesquisador.ex
+++ lib/fuschia/entities/pesquisador.ex
@@ -5,22 +5,22 @@ defmodule Fuschia.Entities.Pesquisador do
   use Fuschia.Schema
   import Ecto.Changeset
 
-  alias Fuschia.Entities.Pesquisador
-  alias Fuschia.Entities.Universidade
-  alias Fuschia.Entities.User
+  alias Fuschia.Entities.{Pesquisador, Universidade, User}
   alias Fuschia.Types.TrimmedString
 
   @required_fields ~w(cpf_usuario minibibliografia tipo_bolsa link_lattes universidade_id)a
-  @optional_fields ~w(cod_orientador)a
+  @optional_fields ~w(orientador_id)a
+
+  @tipos_bolsa ~w(IC pesquisador)
 
   @primary_key {:cpf_usuario, TrimmedString, []}
   schema "pesquisador" do
     field :minibibliografia, :string
     field :tipo_bolsa, :string
     field :link_lattes, :string
-    field :cod_orientador, TrimmedString
+    field :orientador_id, TrimmedString
 
-    has_one :orientador, Pesquisador, references: :cpf_usuario, foreign_key: :cod_orientador
+    has_one :orientador, Pesquisador, references: :cpf_usuario, foreign_key: :orientador_id
     has_many :orientandos, Pesquisador
     belongs_to :usuario, User, references: :cpf, define_field: false, foreign_key: :cpf_usuario
     belongs_to :universidade, Universidade, foreign_key: :universidade_id
@@ -33,8 +33,9 @@ defmodule Fuschia.Entities.Pesquisador do
     struct
     |> cast(attrs, @required_fields ++ @optional_fields)
     |> validate_required(@required_fields)
+    |> validate_inclusion(:tipo_bolsa, @tipos_bolsa)
     |> validate_length(:minibibliografia, max: 280)
-    |> foreign_key_constraint(:cod_orientador)
+    |> foreign_key_constraint(:orientador_id)
     |> foreign_key_constraint(:cpf_usuario)
     |> foreign_key_constraint(:universidade_id)
   end
@@ -42,11 +43,11 @@ defmodule Fuschia.Entities.Pesquisador do
   defimpl Jason.Encoder, for: __MODULE__ do
     def encode(struct, opts) do
       %{
-        cpf_usuario: struct.nome,
+        cpf: struct.cpf_usuario,
         minibibliografia: struct.minibibliografia,
         tipo_bolsa: struct.tipo_bolsa,
         link_lattes: struct.link_lattes,
-        cod_orientador: struct.cod_orientador,
+        orientador_id: struct.orientador_id,
         universidade_id: struct.universidade_id
       }
       |> Fuschia.Encoder.encode(opts)
