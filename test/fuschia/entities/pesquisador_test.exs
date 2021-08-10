1a18844d686a339d991e2fea34a4f1785f81fcc1
Eduardo R <link2755@hotmail.com>
2021-08-10 18:37:27 -0300

    fest: pesquisador_test

---
+++ test/fuschia/entities/pesquisador_test.exs
@@ -0,0 +1,28 @@
+defmodule Fuschia.Entities.PesquisadorTest do
+  use Fuschia.DataCase, async: true
+
+  import Fuschia.Factory
+
+  alias Fuschia.Entities.Pesquisador
+
+  describe "changeset/2" do
+    @invalid_params %{
+      cpf_usuario: nil,
+      minibibliografia: nil,
+      tipo_bolsa: nil,
+      link_lattes: nil,
+      orientador_id: nil,
+      universidade_id: nil
+    }
+
+    test "when all params are valid, return a valid changeset" do
+      default_params = params_for(:pesquisador)
+      assert %Ecto.Changeset{valid?: true} = Pesquisador.changeset(%Pesquisador{}, default_params)
+    end
+
+    test "when params are invalid, return an error changeset" do
+      assert %Ecto.Changeset{valid?: false} =
+               Pesquisador.changeset(%Pesquisador{}, @invalid_params)
+    end
+  end
+end
