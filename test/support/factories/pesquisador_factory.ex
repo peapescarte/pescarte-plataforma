8dc51e110f1080dad6b5c4b6e01e0f2dd8833dd2
Eduardo R <link2755@hotmail.com>
2021-08-11 16:47:05 -0300

    fix: pesquisador factory update

--- test/support/factories/pesquisador_factory.ex
+++ test/support/factories/pesquisador_factory.ex
@@ -6,14 +6,17 @@ defmodule Fuschia.PesquisadorFactory do
       alias Fuschia.Entities.Pesquisador
 
       def pesquisador_factory do
+        user = insert(:user)
+        universidade = insert(:universidade)
+
         %Pesquisador{
-          cpf_usuario: build(:user).cpf,
+          cpf_usuario: user.cpf,
           minibibliografia:
             sequence(:minibibliografia, &"Esta e minha minibibliografia gerada: #{&1}"),
           tipo_bolsa: sequence(:tipo_bolsa, ["ic", "pesquisador"]),
           link_lattes: sequence(:link_lattes, &"http://buscatextual.cnpq.br/buscatextual/:#{&1}"),
           orientador_id: nil,
-          universidade_id: build(:universidade).id
+          universidade_id: universidade.id
         }
       end
     end
