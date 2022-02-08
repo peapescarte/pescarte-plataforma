36fcfd7efa345eacb254f72079a3735b57bca051
zoedsoupe <zoey.spessanha@outlook.com>
2022-02-08 16:07:36 -0300

    better integrate direnv

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = ''
-    Front end para a Plataforma de Gerenciamento de Documentos da Secretaria de Transparência e Controle do Município de Campos dos Goytacazes
+    TODO
   '';
 
   inputs = {
@@ -13,10 +13,10 @@
       let
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
-        elixir_1_13 = pkgs.beam.packages.erlangR24.elixir.override {
-          version = "1.13.1";
+        exDev = pkgs.beam.packages.erlangR24.elixir.override {
+          version = "1.13.2";
           minimumOTPVersion = "24";
-          sha256 = "1f8b63x2klhdz1fq1dgbrqs7x6rq309abzq48gicmd0vprfhc641";
+          sha256 = "qv85aDP3RPCa1YBo45ykWRRZNanL6brNKDMPu9SZdbQ=";
         };
       in {
         devShell = pkgs.mkShell {
@@ -31,10 +31,9 @@
             curl
             libiconv
             # my elixir derivation
-            elixir_1_13
+            exDev
+            beamPackages.rebar3
             glibcLocales
-            nodejs-12_x
-            yarn
             postgresql
           ] ++ pkgs.lib.optional stdenv.isLinux [
                 inotify-tools
@@ -45,11 +44,15 @@
                 CoreFoundation
                 CoreServices
               ]);
+
          # define shell startup command
         shellHook = ''
           # create local tmp folders
           mkdir -p .nix-mix
           mkdir -p .nix-hex
+
+          mix local.hex --force --if-missing
+          mix local.rebar --force --if-missing
 
           # to not conflict with your host elixir
           # version and supress warnings about standard
