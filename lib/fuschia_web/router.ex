a59e6e58740daef8de5b45e4d8644d0f9056dd49
zoedsoupe <zoey.spessanha@outlook.com>
2022-02-21 13:15:45 -0300

    feat: separate public api and browser routes

--- lib/fuschia_web/router.ex
+++ lib/fuschia_web/router.ex
@@ -22,49 +22,16 @@ defmodule FuschiaWeb.Router do
     plug ProperCase.Plug.SnakeCaseParams
   end
 
+  pipeline :api_auth do
+    plug FuschiaWeb.Auth.Pipeline
+  end
+
   pipeline :api_swagger do
     plug :accepts, ["json"]
     plug OpenApiSpex.Plug.PutApiSpec, module: FuschiaWeb.Swagger.ApiSpec
   end
 
-  scope "/" do
-    pipe_through :browser
-
-    get "/swaggerui", OpenApiSpex.Plug.SwaggerUI, path: "/api/openapi"
-
-    live "/example", FuschiaWeb.ExampleLive
-  end
-
-  scope "/api" do
-    pipe_through :api_swagger
-
-    get "/openapi", OpenApiSpex.Plug.RenderSpec, []
-  end
-
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-
-      live_dashboard "/dashboard", metrics: FuschiaWeb.Telemetry
-    end
-  end
-
-  if Mix.env() == :dev do
-    scope "/" do
-      pipe_through :browser
-      surface_catalogue("/catalogue")
-    end
-
-    scope "/dev" do
-      pipe_through :browser
-
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
-  end
-
-  ## Authentication routes
+  ## Endpoints para versão browser
 
   scope "/", FuschiaWeb do
     pipe_through [:browser, :redirect_if_user_is_authenticated]
@@ -102,6 +69,60 @@ defmodule FuschiaWeb.Router do
       post "/confirmar", UserConfirmationController, :create
       get "/confirmar/:token", UserConfirmationController, :edit
       post "/confirmar/:token", UserConfirmationController, :update
+    end
+  end
+
+  scope "/" do
+    pipe_through :browser
+
+    get "/swaggerui", OpenApiSpex.Plug.SwaggerUI, path: "/api/openapi"
+
+    live "/example", FuschiaWeb.ExampleLive
+  end
+
+  ## Endpoints para API pública
+
+  scope "/api/v1" do
+    pipe_through :api_swagger
+
+    get "/openapi", OpenApiSpex.Plug.RenderSpec, []
+  end
+
+  scope "/api/v1", FuschiaWeb do
+    pipe_through :api
+
+    post "/acessar", AuthController, :login
+    post "/cadastrar", AuthController, :signup
+  end
+
+  scope "/api/v1", FuschiaWeb do
+    pipe_through [:api_auth, :api]
+
+    resources "/campi", CampusController, only: [:create, :index, :delete]
+  end
+
+  ## Endpoints para ambiente de desenvolvimento
+
+  if Mix.env() in [:dev, :test] do
+    import Phoenix.LiveDashboard.Router
+
+    scope "/" do
+      pipe_through :browser
+
+      live_dashboard "/dashboard", metrics: FuschiaWeb.Telemetry
+    end
+  end
+
+  if Mix.env() == :dev do
+    scope "/" do
+      pipe_through :browser
+      surface_catalogue("/catalogue")
+    end
+
+    scope "/dev" do
+      pipe_through :browser
+
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
   end
 end
