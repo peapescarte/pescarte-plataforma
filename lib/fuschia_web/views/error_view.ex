38362619181a3f557147d6b259aed44a13092917
Zoey de Souza Pessanha <zoey.spessanha@outlook.com>
2022-07-18 04:52:50 -0300

    fix error view :bug:

--- lib/fuschia_web/views/error_view.ex
+++ lib/fuschia_web/views/error_view.ex
@@ -6,50 +6,11 @@ defmodule FuschiaWeb.ErrorView do
     Ecto.Changeset.traverse_errors(changeset, &translate_error/1)
   end
 
-  @doc """
-  Error for template not found
-  """
-  @spec template_not_found(String.t(), map) :: map
+  def render("500.html", _assigns) do
+    "Erro do servidor"
+  end
+
   def template_not_found(template, _assigns) do
-    %{
-      error: %{
-        details: Phoenix.Controller.status_message_from_template(template)
-      }
-    }
-  end
-
-  @doc """
-  Error for Ecto Changeset validation
-  """
-  @spec render(String.t(), map) :: map
-  def render("error.json", %{changeset: %Ecto.Changeset{} = changeset}) do
-    %{
-      error: %{
-        details: translate_errors(changeset)
-      }
-    }
-  end
-
-  @doc """
-  Error specifying the resource id related
-  """
-  def render("error.json", %{reason: reason, resource_id: resource_id}) do
-    %{
-      error: %{
-        details: reason,
-        resourceId: resource_id
-      }
-    }
-  end
-
-  @doc """
-  Error default with detials only
-  """
-  def render("error.json", %{reason: reason}) do
-    %{
-      error: %{
-        details: reason
-      }
-    }
+    Phoenix.Controller.status_message_from_template(template)
   end
 end
