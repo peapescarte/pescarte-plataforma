ce8bd12878f205dbd5a68978774c7619c41f20b1
Zoey de Souza Pessanha <zoey.spessanha@outlook.com>
2022-12-28 05:44:10 -0300

    feat: remove unused libs

--- mix.exs
+++ mix.exs
@@ -8,25 +8,10 @@ defmodule Pescarte.MixProject do
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [
-        ci: :test,
-        coveralls: :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test
-      ],
       elixirc_options: [warnings_as_errors: true],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps(),
-      dialyzer: dialyzer(),
-      # Documentação
-      name: "Pescarte",
-      source_url: "https://github.com/cciuenf/pescarte",
-      homepage_url: "",
-      docs: [
-        main: "Pescarte",
-        extras: ["README.md"]
-      ]
+      deps: deps()
     ]
   end
 
@@ -40,12 +25,6 @@ defmodule Pescarte.MixProject do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(:dev), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
-
-  defp dialyzer do
-    [
-      plt_add_apps: [:ex_unit]
-    ]
-  end
 
   defp deps do
     [
@@ -62,13 +41,11 @@ defmodule Pescarte.MixProject do
       {:esbuild, "~> 0.3", runtime: Mix.env() == :dev},
       {:ecto_sql, "~> 3.4"},
       {:oban, "~> 2.8"},
-      {:proper_case, "~> 1.0.2"},
       {:nanoid, "~> 2.0.5"},
       {:hackney, "~> 1.8"},
       {:timex, "~> 3.0"},
       {:ex_machina, "~> 2.7.0"},
       {:postgrex, ">= 0.0.0"},
-      {:sentry, "~> 8.0"},
       {:carbonite, "~> 0.4.0"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
@@ -78,10 +55,7 @@ defmodule Pescarte.MixProject do
       {:tesla, "~> 1.4"},
       {:finch, "~> 0.9.0"},
       {:plug_cowboy, "~> 2.3"},
-      {:excoveralls, "~> 0.10", only: [:dev, :test]},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
       {:dart_sass, "~> 0.5", runtime: Mix.env() == :dev}
     ]
@@ -90,7 +64,7 @@ defmodule Pescarte.MixProject do
   defp aliases do
     [
       ci: ["format --check-formatted", "credo --strict", "test"],
-      setup: ["deps.get", "ecto.setup", "assets.build", "dialyzer --format dialyxir"],
+      setup: ["deps.get", "ecto.setup", "assets.build"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "run lib/mix/tasks/seeds.exs", "test"],
