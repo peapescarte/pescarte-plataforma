14390d9d5cfa101abb3d93166835354b7f3f1d17
Zoey de Souza Pessanha <zoey.spessanha@gmail.com>
2023-07-08 12:56:51 -0300

    testes para API zamzar

--- apps/cotacoes_etl/test/test_helper.exs
+++ apps/cotacoes_etl/test/test_helper.exs
@@ -2,4 +2,8 @@ alias CotacoesETL.Integrations.IManagePesagroIntegration
 Mox.defmock(IManagePesagroIntegrationMock, for: IManagePesagroIntegration)
 Application.put_env(:cotacoes_etl, :pesagro_api, IManagePesagroIntegrationMock)
 
+alias CotacoesETL.Integrations.IManageZamzarIntegration
+Mox.defmock(IManageZamzarIntegrationMock, for: IManageZamzarIntegration)
+Application.put_env(:cotacoes_etl, :zamzar_api, IManageZamzarIntegrationMock)
+
 ExUnit.start()
