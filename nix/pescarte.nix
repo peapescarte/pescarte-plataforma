e078afaabdae2f6bbf67615bdbadb6446aec6918
Zoey de Souza Pessanha <zoey.spessanha@gmail.com>
2023-07-26 18:46:21 -0300

    adiciona build com nix

---
+++ nix/pescarte.nix
@@ -0,0 +1,36 @@
+{
+  mixRelease,
+  fetchMixDeps,
+  erlang,
+  nix-gitignore,
+  nodeDependencies,
+}: let
+  src = nix-gitignore.gitignoreSource [../.gitignore] ./..;
+in
+  mixRelease rec {
+    inherit src;
+    pname = "pescarte";
+    version = "production";
+    mixFodDeps = fetchMixDeps {
+      inherit version;
+      pname = "${pname}-deps";
+      inherit src;
+      sha256 = "8yS5gtFzmgl8mV2nNJzYbdxxyqf/P30XougCMprGFa8=";
+    };
+    postBuild = ''
+      ln -sf ${nodeDependencies}/lib/node_modules apps/plataforma_digital/assets/node_modules
+      mix do deps.loadpaths --no-deps-check, phx.digest
+    '';
+    installPhase = ''
+      runHook preInstall
+      mix release ${pname} --no-deps-check --path "$out"
+      runHook postInstall
+    '';
+    preFixup = ''
+      for script in $out/releases/*/elixir; do
+        substituteInPlace "$script" --replace 'ERL_EXEC="erl"' 'ERL_EXEC="${erlang}/bin/erl"'
+      done
+
+      wrapProgram $out/bin/pescarte --set RELEASE_COOKIE ${pname}
+    '';
+  }
