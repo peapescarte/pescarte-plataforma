df2fd26f93570cf62fd8c7af1ae41d3a2752a7e4
Zoey de Souza Pessanha <zoey.spessanha@gmail.com>
2023-07-28 15:57:06 -0300

    finaliza componente de busca

--- apps/plataforma_digital/lib/plataforma_digital/design_system/search_input.ex
+++ apps/plataforma_digital/lib/plataforma_digital/design_system/search_input.ex
@@ -1,36 +1,43 @@
 defmodule PlataformaDigital.DesignSystem.SearchInput do
   use Phoenix.LiveComponent
+
   alias PlataformaDigital.DesignSystem
 
   @impl true
   def mount(socket) do
-    {:ok, assign(socket, typing?: false)}
+    {:ok,
+     socket
+     |> assign(typing?: true)
+     |> assign(filtered: [])}
   end
 
   attr :id, :string, required: true
   attr :name, :string, required: true
   attr :content, :list, default: []
   attr :placeholder, :string, default: "Faça uma pesquisa..."
-  attr :field, Phoenix.HTML.FormField
+  attr :field, Phoenix.HTML.FormFieldcontent
+  attr :size, :string, values: ~w(base large), default: "base"
 
   @impl true
   def render(assigns) do
     ~H"""
-    <fieldset class={["search-input", if(@typing?, do: "typing", else: "empty")]}>
-      <span class="search-icon">
-        <Lucideicons.search />
-      </span>
-      <input
-        id={@id}
-        name={@name}
-        list={@id <> "_data_list"}
-        placeholder={@placeholder}
-        phx-keyup="search"
-        phx-target={@myself}
-        phx-debounce={300}
-      />
-      <ul class={["search-menu", if(@typing?, do: "block", else: "hidden")]}>
-        <li :for={option <- @content} class="search-menu-option">
+    <div class={["search-input-wrapper", @size]}>
+      <fieldset class={["search-input", if(@typing?, do: "typing", else: "empty")]}>
+        <span class="search-icon">
+          <Lucideicons.search />
+        </span>
+        <input
+          id={@id}
+          name={@name}
+          list={@id <> "_data_list"}
+          placeholder={@placeholder}
+          phx-keyup="search"
+          phx-target={@myself}
+          phx-debounce={100}
+        />
+      </fieldset>
+      <ul class={["search-menu", if(@typing?, do: "show", else: "hide")]}>
+        <li :for={option <- maybe_render_content(@content, @filtered)} class="search-menu-option">
           <.link patch="/">
             <DesignSystem.text size="lg">
               <%= option %>
@@ -38,22 +45,36 @@ defmodule PlataformaDigital.DesignSystem.SearchInput do
           </.link>
         </li>
       </ul>
-    </fieldset>
+    </div>
     """
   end
 
   @impl true
-  def handle_event("search", %{"value" => value}, socket) do
-    content = maybe_display_matches(socket.assigns.content, value)
-
+  def handle_event("search", %{"key" => "Backspace", "value" => ""}, socket) do
     {:noreply,
      socket
-     |> assign(typing?: true)
-     |> assign(content: content)}
+     |> assign(typing?: false)
+     |> assign(filtered: [])}
+  end
+
+  def handle_event("search", %{"value" => value}, socket) do
+    case maybe_display_matches(socket.assigns.content, value) do
+      [] ->
+        {:noreply, socket}
+
+      filtered ->
+        {:noreply,
+         socket
+         |> assign(typing?: true)
+         |> assign(filtered: filtered)}
+    end
   end
 
   defp maybe_display_matches(content, value) do
     matches = Enum.filter(content, &(&1 =~ value))
     if Enum.empty?(matches), do: content, else: matches
   end
+
+  defp maybe_render_content(content, []), do: content
+  defp maybe_render_content(_content, filtered), do: filtered
 end
