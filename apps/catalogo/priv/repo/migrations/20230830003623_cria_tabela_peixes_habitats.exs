488a68ea9752e9debf063cbb7b4fe8c10966cd31
Alex Momi <alexmomijunior@gmail.com>
2023-09-04 08:28:13 -0300

    fix: Adicionando o app catalogo nas migracoes

--- apps/catalogo/priv/repo/migrations/20230830003623_cria_tabela_peixes_habitats.exs
+++ apps/catalogo/priv/repo/migrations/20230830003623_cria_tabela_peixes_habitats.exs
@@ -5,5 +5,6 @@ defmodule Catalogo.Repo.Migrations.CriaTabelaPeixesHabitats do
     create table(:peixes_habitats, primary_key: false) do
       add :peixe_nome_cientifico, references(:peixe, column: :nome_cientifico, type: :string), primary_key: true
       add :habitat_nome, references(:habitat, column: :nome, type: :string), primary_key: true
+    end
   end
 end
