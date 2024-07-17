4dd8213ebc119db70ca7313d01ebb6667c574584
douglastofoli <tofoli.douglas@hotmail.com>
2024-07-17 17:09:17 -0300

    fix: add new assign current_path

--- lib/pescarte_web/controllers/aboutus_controller.ex
+++ lib/pescarte_web/controllers/aboutus_controller.ex
@@ -2,6 +2,7 @@ defmodule PescarteWeb.AboutUsController do
   use PescarteWeb, :controller
 
   def show(conn, _params) do
-    render(conn, :show)
+    current_path = conn.request_path
+    render(conn, :show, current_path: current_path, error_message: nil)
   end
 end
