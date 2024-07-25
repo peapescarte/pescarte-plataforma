a8a011ac193fee038db86ae2e520030b47056b01
Zoey de Souza Pessanha <zoey.spessanha@zeetech.io>
2024-07-25 12:10:07 -0300

    fix: links to internal pages

--- lib/pescarte_web/templates/landing_html.ex
+++ lib/pescarte_web/templates/landing_html.ex
@@ -2,10 +2,4 @@ defmodule PescarteWeb.LandingHTML do
   use PescarteWeb, :html
 
   embed_templates("landing_html/*")
-
-  def handle_event("dialog", _value, socket) do
-    IO.puts("HHHHHHHHEEEEEEEEELLLLLLLLLOOOOOOOOOOOOOOOOOOOOO")
-
-    {:noreply, socket}
-  end
 end
