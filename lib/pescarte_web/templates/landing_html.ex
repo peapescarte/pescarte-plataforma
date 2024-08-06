25516c61d63d4187d58beacc5ad3267e36ae6aac
juanzeen <jziignerdev@gmail.com>
2024-08-06 15:39:29 -0300

    news alterations in the landing finished

--- lib/pescarte_web/templates/landing_html.ex
+++ lib/pescarte_web/templates/landing_html.ex
@@ -2,4 +2,12 @@ defmodule PescarteWeb.LandingHTML do
   use PescarteWeb, :html
 
   embed_templates("landing_html/*")
+
+  def handle_notice_text_length(text, max_length) do
+    case String.length(text) do
+    text_length when (text_length >= max_length) ->
+    String.slice(text, 0..max_length - 4) <> "..."
+    _text_length -> text
+    end
+  end
 end
