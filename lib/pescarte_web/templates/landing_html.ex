d2b7d5bbccf4ae28ed15fd11c4a4e1edd8d95a8e
juanzeen <jziignerdev@gmail.com>
2024-08-06 19:14:17 -0300

    upgrade on the text format

--- lib/pescarte_web/templates/landing_html.ex
+++ lib/pescarte_web/templates/landing_html.ex
@@ -5,9 +5,16 @@ defmodule PescarteWeb.LandingHTML do
 
   def handle_notice_text_length(text, max_length) do
     case String.length(text) do
-    text_length when (text_length >= max_length) ->
-    String.slice(text, 0..max_length - 4) <> "..."
-    _text_length -> text
+      text_length when text_length >= max_length ->
+        text =
+          text
+          |> String.slice(0..(max_length - 4))
+          |> String.trim_trailing()
+
+        text <> "..."
+
+      _text_length ->
+        text
     end
   end
 end
