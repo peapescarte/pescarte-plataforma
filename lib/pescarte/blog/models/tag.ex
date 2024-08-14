f64dffe07b729ff45ec0a224449d359d4e9edb7b
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2024-08-14 21:34:46 +0000

    feat: created blog_tag model

---
+++ lib/pescarte/blog/models/tag.ex
@@ -0,0 +1,24 @@
+defmodule Pescarte.Blog.Models.Tag do
+  use Pescarte, :model
+
+  alias Pescarte.Database.Types.PublicId
+
+  @type t :: %Tag{nome: binary, id: binary}
+
+  @required_fields ~w(nome)a
+
+  @primary_key {:id, PublicId, autogenerate: true}
+  schema "blog_tag" do
+    field :nome, :string
+
+    timestamps()
+  end
+
+  @spec changeset(Tag.t(), map) :: changeset
+  def changeset(%Tag{} = tag, attrs) do
+    tag
+    |> cast(attrs, @required_fields)
+    |> validate_required(@required_fields)
+    |> unique_constraint(:nome)
+  end
+end
