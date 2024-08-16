c234efbc2d0965676b23bc5c7a9dbef92f3b7369
douglastofoli <tofoli.douglas@hotmail.com>
2024-08-16 12:17:27 -0300

    feat: add images

--- lib/pescarte_web/controllers/equipes_controller.ex
+++ lib/pescarte_web/controllers/equipes_controller.ex
@@ -3,15 +3,21 @@ defmodule PescarteWeb.EquipesController do
 
   def show(conn, _params) do
     current_path = conn.request_path
+    linhas_pesquisas = get_data_from_file("linhas_pesquisas.json")
+    membros = get_data_from_file("membros.json")
 
-    {:ok, data} =
-      "equipes"
-      |> Pescarte.get_static_file_path("equipes.json")
-      |> File.read!()
-      |> Jason.decode()
+    render(conn, :show,
+      linhas_pesquisas: linhas_pesquisas,
+      membros: membros,
+      error_message: nil,
+      current_path: current_path
+    )
+  end
 
-    Pescarte.Supabase.Storage.list_buckets()
-
-    render(conn, :show, data: data, error_message: nil, current_path: current_path)
+  defp get_data_from_file(file_name) do
+    "equipes"
+    |> Pescarte.get_static_file_path(file_name)
+    |> File.read!()
+    |> Jason.decode!()
   end
 end
