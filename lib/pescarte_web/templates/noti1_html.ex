7a787731d697cde36d268fe91f51024ead131374
douglastofoli <tofoli.douglas@hotmail.com>
2024-08-16 13:19:25 -0300

    mix format

--- lib/pescarte_web/templates/noti1_html.ex
+++ lib/pescarte_web/templates/noti1_html.ex
@@ -2,8 +2,8 @@ defmodule PescarteWeb.Noti1HTML do
   use PescarteWeb, :html
 
   embed_templates("noti1_html/*")
-  
-def handle_event("dialog", _value, socket) do
-  {:noreply, socket}
-end
+
+  def handle_event("dialog", _value, socket) do
+    {:noreply, socket}
+  end
 end
--- lib/pescarte_web/templates/noticias_html.ex
+++ lib/pescarte_web/templates/noticias_html.ex
@@ -4,44 +4,44 @@ defmodule PescarteWeb.NoticiasHTML do
   embed_templates("noticias_html/*")
 
   @notice_title_max_length Application.compile_env!(:pescarte, [
-    PescarteWeb,
-    :notice_title_max_length
-  ])
+                             PescarteWeb,
+                             :notice_title_max_length
+                           ])
 
-@notice_desc_max_length Application.compile_env!(:pescarte, [
-   PescarteWeb,
-   :notice_desc_max_length
- ])
+  @notice_desc_max_length Application.compile_env!(:pescarte, [
+                            PescarteWeb,
+                            :notice_desc_max_length
+                          ])
 
-def handle_notice_title_length(text) do
-  if String.length(text) > @notice_title_max_length do
-    text
-    |> truncate_text_until(@notice_title_max_length - 4)
-    |> put_ellipsis()
-  else
-    text
+  def handle_notice_title_length(text) do
+    if String.length(text) > @notice_title_max_length do
+      text
+      |> truncate_text_until(@notice_title_max_length - 4)
+      |> put_ellipsis()
+    else
+      text
+    end
   end
-end
 
-def handle_notice_desc_length(text) do
-  if String.length(text) > @notice_desc_max_length do
-    text
-    |> truncate_text_until(@notice_desc_max_length - 4)
-    |> put_ellipsis()
-  else
-    text
+  def handle_notice_desc_length(text) do
+    if String.length(text) > @notice_desc_max_length do
+      text
+      |> truncate_text_until(@notice_desc_max_length - 4)
+      |> put_ellipsis()
+    else
+      text
+    end
   end
-end
 
-defp truncate_text_until(text, length) do
-  text
-  |> String.slice(0..length)
-  |> String.trim_trailing()
-end
+  defp truncate_text_until(text, length) do
+    text
+    |> String.slice(0..length)
+    |> String.trim_trailing()
+  end
 
-defp put_ellipsis(text) do
-  text <> "..."
-end
+  defp put_ellipsis(text) do
+    text <> "..."
+  end
 
   def handle_event("dialog", _value, socket) do
     {:noreply, socket}
