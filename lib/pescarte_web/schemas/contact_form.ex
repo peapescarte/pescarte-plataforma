64c59cda98ab1a553a9645a369b361a076a2b925
Rafael Mori <103967050+Rafael-Mori-2022@users.noreply.github.com>
2024-08-28 15:29:15 -0300

    fix: correct receiver and sender email addresses and generate confirmation email (#193)

--- lib/pescarte_web/schemas/contact_form.ex
+++ lib/pescarte_web/schemas/contact_form.ex
@@ -4,18 +4,28 @@ defmodule PescarteWeb.ContactForm do
 
   @primary_key false
   embedded_schema do
-    field :sender_name, :string
-    field :sender_email, :string
-    field :sender_option, :string
-    field :sender_message, :string
+    field :form_sender_name, :string
+    field :form_sender_email, :string
+    field :form_sender_option, :string
+    field :form_sender_message, :string
   end
 
   def changeset(params) do
     %__MODULE__{}
-    |> cast(params, [:sender_name, :sender_email, :sender_option, :sender_message])
-    |> validate_required([:sender_name, :sender_email, :sender_option, :sender_message])
-    |> validate_format(:sender_email, ~r/@/)
-    |> validate_length(:sender_message, min: 1)
+    |> cast(params, [
+      :form_sender_name,
+      :form_sender_email,
+      :form_sender_option,
+      :form_sender_message
+    ])
+    |> validate_required([
+      :form_sender_name,
+      :form_sender_email,
+      :form_sender_option,
+      :form_sender_message
+    ])
+    |> validate_format(:form_sender_email, ~r/@/)
+    |> validate_length(:form_sender_message, min: 1)
   end
 
   def apply_action_changeset(params, action) do
