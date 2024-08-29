557c8a824ae92541ce5f91436eb09068f152b25a
douglastofoli <tofoli.douglas@hotmail.com>
2024-08-29 09:11:16 -0300

    chore: remove unused functions and var

--- lib/pescarte_web/controllers/contact_controller.ex
+++ lib/pescarte_web/controllers/contact_controller.ex
@@ -3,7 +3,6 @@ defmodule PescarteWeb.ContactController do
   require Logger
   alias PescarteWeb.ContactForm
 
-  @sender_email Application.compile_env!(:pescarte, [PescarteWeb, :sender_email])
   @receiver_email Application.compile_env!(:pescarte, [PescarteWeb, :receiver_email])
 
   def show(conn, _params) do
@@ -57,37 +56,5 @@ defmodule PescarteWeb.ContactController do
         |> put_flash(:error, "Erro na validação do formulário.")
         |> redirect(to: ~p"/")
     end
-  end
-
-  defp send_email_to_pescarte(client, contact_form) do
-    email_data_to_pescarte = %{
-      from: @sender_email,
-      to: @receiver_email,
-      subject: contact_form.form_sender_option,
-      html: """
-      <p><strong>Nome:</strong> #{contact_form.form_sender_name}</p>
-      <p><strong>Assunto:</strong> #{contact_form.form_sender_option}</p>
-      <p><strong>Mensagem:</strong> #{contact_form.form_sender_message}</p>
-      """
-    }
-
-    Resend.Emails.send(client, email_data_to_pescarte)
-  end
-
-  defp send_confirmation_email(client, contact_form) do
-    email_data_to_form_sender = %{
-      from: @sender_email,
-      to: contact_form.form_sender_email,
-      subject: "Confirmação de recebimento do formulário",
-      html: """
-      <p>Olá, #{contact_form.form_sender_name},</p>
-      <p>Recebemos seu formulário com o assunto: <strong>#{contact_form.form_sender_option}</strong>.</p>
-      <p>Em breve retornaremos sua mensagem.</p>
-      <p>Atenciosamente,</p>
-      <p>Equipe Pescarte</p>
-      """
-    }
-
-    Resend.Emails.send(client, email_data_to_form_sender)
   end
 end
