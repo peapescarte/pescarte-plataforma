17e4359936de43ae8cca4c40d2fd606f2e4072cd
douglastofoli <tofoli.douglas@hotmail.com>
2024-09-16 10:48:59 -0300

    get members images from supabase

--- lib/pescarte_web/controllers/equipes_controller.ex
+++ lib/pescarte_web/controllers/equipes_controller.ex
@@ -1,10 +1,17 @@
 defmodule PescarteWeb.EquipesController do
   use PescarteWeb, :controller
 
+  alias Pescarte.Supabase.Storage
+  alias Supabase.Storage.Bucket
+
   def show(conn, _params) do
     current_path = conn.request_path
     linhas_pesquisas = get_data_from_file("linhas_pesquisas.json")
-    membros = get_data_from_file("membros.json")
+
+    membros =
+      "membros.json"
+      |> get_data_from_file()
+      |> get_images()
 
     nucleo_norte = [
       "Equipe de Campo - Campos dos Goytacazes",
@@ -36,5 +43,34 @@ defmodule PescarteWeb.EquipesController do
     |> Pescarte.get_static_file_path(file_name)
     |> File.read!()
     |> Jason.decode!()
+  end
+
+  defp get_images(membros) do
+    Enum.into(membros, %{}, fn {nucleo, membros} ->
+      {nucleo, Enum.map(membros, &update_image/1)}
+    end)
+  end
+
+  defp update_image(membro) do
+    Map.update(membro, "imagem", nil, &create_signed_url/1)
+  end
+
+  defp create_signed_url(file_name) do
+    result =
+      Storage.create_signed_url(
+        %Bucket{name: "static"},
+        "images/equipes/membros/#{file_name}",
+        3600
+      )
+
+    case result do
+      {:ok, url} -> add_storage_suffix(url)
+      {:error, _reason} -> ""
+    end
+  end
+
+  defp add_storage_suffix(url) do
+    [protocol_and_domain, path] = String.split(url, "/object/", parts: 2)
+    "#{protocol_and_domain}/storage/v1/object/#{path}"
   end
 end
