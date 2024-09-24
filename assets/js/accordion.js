9c13fa0d9293f9b77a362c60e321f5797193db05
Rafael-Mori-2022 <rafaelmp2018@icloud.com>
2024-09-23 22:09:24 -0300

    wip: add temporary content

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,34 +1,16 @@
 const phases = document.querySelectorAll('.phases');
 const units = document.querySelectorAll('.units-accordions');
 
-units.forEach(phase => {
-    const accordions = phase.getElementsByClassName('accordion-bx');
-    const image = phase.querySelector('img');
-    const initHeight = image.scrollHeight;
+units.forEach(unit => {
+    const accordions = unit.getElementsByClassName('accordion-bx');
     
-    function adjustImageHeight() {
-        const windowWidth = window.innerWidth;
-        
-        if (windowWidth > 768) {
-            let newHeight = initHeight;
-            Array.from(accordions).forEach(accordion => {
-                if (accordion.classList.contains('active')) {
-                    const phaseContentHeight = phase.querySelector('.production-units').scrollHeight;
-                    newHeight = phaseContentHeight;
-                }
-            });
-            image.style.height = `${newHeight}px`;
-        }
-    }
-
     for (let i = 0; i < accordions.length; i++) {
         accordions[i].addEventListener('click', function() {
+          
             if (!this.classList.contains('active')) {
                 this.classList.add('active');
-                adjustImageHeight();
             } else {
                 this.classList.remove('active');
-                adjustImageHeight();
             }
         });
     }
