0997ac329e5f6a8e59a3c878c88befb0e5248068
Rafael Mori <103967050+Rafael-Mori-2022@users.noreply.github.com>
2024-09-25 16:29:43 -0300

    feat/pgtr (#224)
    * extract common tasks aliases
    
    * wip: add 'pgtr' page with missing content
    
    * wip: add temporary content
    
    * feat: 'pgtr' page
    
    * wip: add 'pgtr' page with missing content
    
    * wip: add temporary content
    
    * feat: 'pgtr' page
    
    * fix: accordion.js
    
    ---------
    
    Co-authored-by: Zoey de Souza Pessanha <zoey.spessanha@zeetech.io>

---
+++ lib/pescarte_web/controllers/pgtrs_controller.ex
@@ -0,0 +1,8 @@
+defmodule PescarteWeb.PGTRSController do
+  use PescarteWeb, :controller
+
+  def show(conn, _params) do
+    current_path = conn.request_path
+    render(conn, :show, current_path: current_path, error_message: nil)
+  end
+end
