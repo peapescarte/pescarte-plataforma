b9eb8235d64c1ee505e5f48eec3dfd0dd458d676
Rafael-Mori-2022 <rafaelmp2018@icloud.com>
2024-09-25 15:48:47 -0300

    fix: accordion.js

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -16,43 +16,13 @@ units.forEach(unit => {
         });
     }
 });
-const units = document.querySelectorAll('.units-accordions');
-
-units.forEach(unit => {
-    const accordions = unit.getElementsByClassName('accordion-bx');
-    
-    for (let i = 0; i < accordions.length; i++) {
-        const label = accordions[i].querySelector('.accordion-label');
-
-        label.addEventListener('click', function() {
-            if (!accordions[i].classList.contains('active')) {
-                accordions[i].classList.add('active');
-            } else {
-                accordions[i].classList.remove('active');
-            }
-        });
-    }
-});
 
 phases.forEach(phase => {
     const accordions = phase.getElementsByClassName('accordion-bx');
     const image = phase.querySelector('img');
     const initHeight = image.scrollHeight;
     
-    
     function adjustImageHeight() {
-        const windowWidth = window.innerWidth;
-        
-        if (windowWidth > 768) {
-            let newHeight = initHeight;
-            Array.from(accordions).forEach(accordion => {
-                if (accordion.classList.contains('active')) {
-                    const phaseContentHeight = phase.querySelector('.phase-content').scrollHeight;
-                    newHeight = phaseContentHeight;
-                }
-            });
-            image.style.height = `${newHeight}px`;
-        }
         const windowWidth = window.innerWidth;
         
         if (windowWidth > 768) {
