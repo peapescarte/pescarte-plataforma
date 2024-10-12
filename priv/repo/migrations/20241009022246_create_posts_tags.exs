44832ed67203f635f3959a4b8334844cd0669b5a
juanzeen <jziignerdev@gmail.com>
2024-10-12 20:17:09 -0300

    fix: fixing tag reference in post_tags migration

--- priv/repo/migrations/20241009022246_create_posts_tags.exs
+++ priv/repo/migrations/20241009022246_create_posts_tags.exs
@@ -4,7 +4,7 @@ defmodule Pescarte.Database.Repo.Migrations.CreatePostTags do
   def change do
     create table(:posts_tags) do
       add :post_id, references(:posts, type: :string), null: false
-      add :tag_id, references(:tag, type: :string), null: false
+      add :tag_id, references(:blog_tag, type: :string), null: false
       timestamps()
     end
   end
