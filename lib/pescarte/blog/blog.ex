c5545f8a112e4e0cd3a2b0d0e91b4821679ba4da
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2024-10-24 21:56:17 -0300

    feat: added pagination and default functions to handle missing filters

--- lib/pescarte/blog/blog.ex
+++ lib/pescarte/blog/blog.ex
@@ -10,7 +10,9 @@ defmodule Pescarte.Blog do
     optional(:title) => String.t(),
     optional(:start_date) => NaiveDateTime.t(),
     optional(:end_date) => NaiveDateTime.t(),
-    optional(:tags) => list(Tag.t())
+    optional(:tags) => list(Tag.t()),
+    optional(:page) => non_neg_integer(),
+    optional(:page_size) => pos_integer()
   }
 
   @spec list_posts_with_filter(filters()) :: {:ok, list(Post.t())} | {:error, term()}
@@ -18,14 +20,24 @@ defmodule Pescarte.Blog do
     Post
     |> apply_post_search_filter(filters)
     |> apply_post_date_filter(filters)
+    |> apply_pagination(filters)
     |> Repo.replica().all()
   end
 
   defp apply_post_search_filter(query, %{title: search_term}) do
     from(p in query, where: ilike(p.titulo, ^"%#{search_term}%"))
   end
+  defp apply_post_search_filter(query, _), do: query
 
   defp apply_post_date_filter(query, %{start_date: start_date, end_date: end_date}) do
     from p in query, where: p.inserted_at >= ^start_date and p.inserted_at <= ^end_date
   end
+  defp apply_post_date_filter(query, _), do: query
+
+  defp apply_pagination(query, %{page, page_size}) do
+    offset = (page - 1) * page_size
+    from p in query, limit: ^page_size, offset: ^offset
+  end
+  defp apply_pagination(query, _), do: (from p in query, limit: 10, offset: 0)
+
 end
