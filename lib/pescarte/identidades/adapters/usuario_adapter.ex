55ca8d537164c841f7e9272bbc2860204c66a4ba
zoeyrinha <zoey.spessanha@zeetech.io>
2024-12-20 09:01:32 -0300

    Atualizar elixir para 1.18 (#247)
    * upgrade: elixir nix
    
    * fix: type check errors
    
    * upgrade: elixir in infra

--- lib/pescarte/identidades/adapters/usuario_adapter.ex
+++ lib/pescarte/identidades/adapters/usuario_adapter.ex
@@ -5,6 +5,28 @@ defmodule Pescarte.Identidades.Adapters.UsuarioAdapter do
 
   @contato_fields ~w(email_principal celular_principal emails_adicionais celulares_adicionais endereco)a
 
+  @type external :: %{
+          role: String.t(),
+          phone: String.t(),
+          email: String.t(),
+          password: String.t(),
+          app_metadata: %{
+            cpf: String.t(),
+            rg: String.t(),
+            contato: %{
+              email_principal: String.t(),
+              celular_principal: String.t(),
+              emails_adicionais: list(String.t()),
+              celulares_adicionais: list(String.t()),
+              endereco: String.t()
+            },
+            primeiro_nome: String.t(),
+            sobrenome: String.t(),
+            nome_completo: String.t()
+          }
+        }
+
+  @spec to_external(Usuario.t(), String.t()) :: external
   def to_external(%Usuario{} = usuario, pass \\ @default_pass) do
     %{
       role: Atom.to_string(usuario.papel),
