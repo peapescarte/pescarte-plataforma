30beabd9bd0b53984ade223e1479951ef685f3c8
zoeyrinha <zoey.spessanha@zeetech.io>
2024-12-20 07:25:17 -0300

    Revert "Feat/core create post (#234)" (#245)
    This reverts commit 8cb6b4427aaf85720b38dfdabb9e26cd97070750.

--- lib/pescarte/blog/post.ex
+++ lib/pescarte/blog/post.ex
@@ -2,8 +2,6 @@ defmodule Pescarte.Blog.Post do
   @moduledoc """
   Módulo que define o schema e o changeset para os posts.
   """
-  alias Ecto.Multi
-  alias Pescarte.Blog.Entity.Tag
   alias Pescarte.Database
   alias Pescarte.Database.Repo
   alias Pescarte.Database.Types.PublicId
@@ -18,32 +16,33 @@ defmodule Pescarte.Blog.Post do
           published_at: NaiveDateTime.t(),
           inserted_at: NaiveDateTime.t(),
           updated_at: NaiveDateTime.t(),
-          usuario: Usuario.t()
+          usuario: Usuario.t(),
+          usuario_id: String.t()
         }
 
-  @required_params [:titulo, :conteudo, :link_imagem_capa, :published_at, :usuario_id]
+  @required_params [:titulo, :conteudo, :link_imagem_capa, :published_at]
 
   @primary_key {:id, PublicId, autogenerate: true}
-  schema "blog_posts" do
+  schema "posts" do
     field :titulo, :string
     field :conteudo, :binary
     field :link_imagem_capa, :string
     field :published_at, :naive_datetime
 
-    belongs_to :usuario, Usuario, type: :string
-    many_to_many :blog_tags, Tag, join_through: "posts_tags"
+    belongs_to :usuario, Usuario
+
+    # comentado enquanto o PR das tags não é aprovado
+    # many_to_many :tags, Tag, through: [:post_tags, :tag]
 
     timestamps()
   end
 
-  @spec changeset(Post.t(), map) :: changeset
+  @spec changeset(t, map) :: changeset
   def changeset(post \\ %Post{}, params) do
     post
-    |> Map.put(:published_at, NaiveDateTime.local_now())
     |> cast(params, @required_params)
     |> validate_required(@required_params)
     |> unique_constraint(:titulo)
-    |> foreign_key_constraint(:usuario_id)
   end
 
   @spec get_posts :: list(Post.t()) | Ecto.QueryError
@@ -56,15 +55,11 @@ defmodule Pescarte.Blog.Post do
     Database.fetch(Post, id)
   end
 
-  @spec create_post(Map) :: {:ok, Post.t()} | {:error, Ecto.Changeset.t()}
-  def create_post(%{blog_tags: tags} = params) do
-    Multi.new()
-    |> Multi.insert_all(:update_tags, Tag, tags,
-      on_conflict: :replace_all,
-      conflict_target: :nome
-    )
-    |> Multi.insert(:blog_posts, changeset(%Post{}, params))
-    |> Repo.transaction()
+  @spec create_post(Post.t()) :: {:ok, Post.t()} | {:error, Ecto.Changeset.t()}
+  def create_post(params) do
+    %Post{}
+    |> Post.changeset(params)
+    |> Repo.insert()
   end
 
   @spec delete_post(String.t()) :: {:ok, Post.t()} | {:error, :not_found}
