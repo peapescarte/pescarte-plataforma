7f34642e1fac1e27bda54dc993c5f9e20e70b3ca
douglastofoli <tofoli.douglas@hotmail.com>
2025-03-01 11:42:37 -0300

    create function to handle url from storage by passing a path string

--- lib/pescarte/supabase.ex
+++ lib/pescarte/supabase.ex
@@ -1,3 +1,21 @@
 defmodule Pescarte.Supabase do
   use Supabase.Client, otp_app: :pescarte
+
+  alias Supabase.Storage
+  alias Supabase.Storage.File
+
+  @spec retrieve_url_from_storage(String.t()) :: {:ok, String.t()} | {:error, String.t()}
+  def retrieve_url_from_storage(path) do
+    bucket_id = "static"
+
+    with {:ok, client} <- get_client(),
+         {:ok, url} <-
+           File.create_signed_url(%Storage{bucket_id: bucket_id, client: client}, path,
+             expires_in: 300_000
+           ) do
+      {:ok, url}
+    else
+      {:error, _} -> {:error, "Failed to retrieve URL"}
+    end
+  end
 end
