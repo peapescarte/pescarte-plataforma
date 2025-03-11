5571d4f16656946dae19a55730cc8be3bce9238c
douglastofoli <tofoli.douglas@hotmail.com>
2025-03-11 14:22:52 -0300

    refactor: extract PDF URL generation into a separate function
    - Moved the logic for generating public document URLs into `get_public_document_url/0`.
    - Improved code readability and maintainability by separating concerns.
    - Ensured the `show/2` function remains clean and focused on rendering.

--- lib/pescarte_web/controllers/boletins_controller.ex
+++ lib/pescarte_web/controllers/boletins_controller.ex
@@ -2,20 +2,23 @@ defmodule PescarteWeb.BoletinsController do
   use PescarteWeb, :controller
 
   def show(conn, _params) do
+    pdf_urls = get_public_document_url()
+
+    render(conn, :show, current_path: conn.request_path, pdf_url: pdf_urls, error_message: nil)
+  end
+
+  defp get_public_document_url do
     pdf_paths =
       for i <- 1..8, do: "publicacoes/boletins/boletim%20#{i}/Boletim%20Pescarte%200#{i}.pdf"
 
-    pdf_urls =
-      pdf_paths
-      |> Enum.with_index(1)
-      |> Enum.map(fn {link, index} ->
-        case Pescarte.Storage.get_public_area_image_url(link) do
-          {:ok, url} -> {"boletim#{index}", url}
-          _ -> {"boletim#{index}", nil}
-        end
-      end)
-      |> Enum.into(%{})
-
-    render(conn, :show, current_path: conn.request_path, pdf_url: pdf_urls, error_message: nil)
+    pdf_paths
+    |> Enum.with_index(1)
+    |> Enum.map(fn {link, index} ->
+      case Pescarte.Storage.get_public_area_image_url(link) do
+        {:ok, url} -> {"boletim#{index}", url}
+        _ -> {"boletim#{index}", nil}
+      end
+    end)
+    |> Enum.into(%{})
   end
 end
