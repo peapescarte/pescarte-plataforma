a26839d48ffad4d2ce7bd6eb729ccfa28be8b7a0
douglastofoli <tofoli.douglas@hotmail.com>
2025-04-07 10:42:58 -0300

    chore: fix typo

--- lib/pescarte_web/router.ex
+++ lib/pescarte_web/router.ex
@@ -67,7 +67,7 @@ defmodule PescarteWeb.Router do
       get "/artigos", ArticleController, :show
       get "/livros", LivrosController, :show
       get "/boletins", BoletinsController, :show
-      get "/georreferenciamentos", GeorreferecimentoController, :show
+      get "/georreferenciamentos", GeorreferenciamentosController, :show
       get "/relato_publico", RelatoPublicoController, :show
     end
 
